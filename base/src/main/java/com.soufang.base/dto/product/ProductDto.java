7b00c3a47ecd1f56302137f6a57606e69b061061
Loulq <330396052@qq.com>
2019-06-15 13:50:45 +0800

    搜索列表 bug修改提交

--- base/src/main/java/com.soufang.base/dto/product/ProductDto.java
+++ base/src/main/java/com.soufang.base/dto/product/ProductDto.java
@@ -41,6 +41,7 @@ public class ProductDto extends PageBase implements Serializable {
 
     private String productImage;
     private String productUrl;
+    private String url;
 
     private Integer productLevel;
 
@@ -96,10 +97,9 @@ public class ProductDto extends PageBase implements Serializable {
         return PropertiesParam.file_pre + productImage;
     }
 
-//    public String getUrl() {
-//        return productImage == null || "".equals(productImage) ? "" : PropertiesParam.file_pre + productImage.substring(0, productImage.indexOf(";"));
-//    }
-
+    public String getUrl() {
+        return productImage == null || "".equals(productImage) ? "" : productImage.indexOf(";")!= -1 ? PropertiesParam.file_pre + productImage.substring(0, productImage.indexOf(";")) : PropertiesParam.file_pre + productImage ;
+    }
 
     // 产品具体对应规格数量的信息
     private ProductSpecDto productSpecDto;
