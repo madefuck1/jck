e1cd745111878888aaa63f41c4d7ea1e2302b73c
ChenJevon <504987307@qq.com>
2019-06-24 16:02:48 +0800

    1

--- core/src/main/java/com/soufang/service/impl/ShopServiceImpl.java
+++ core/src/main/java/com/soufang/service/impl/ShopServiceImpl.java
@@ -32,6 +32,7 @@ public class ShopServiceImpl implements ShopService {
         Shop shop = shopMapper.getById(id);
         ShopDto shopDto = new ShopDto();
         BeanUtils.copyProperties(shop, shopDto);
+        shopDto.setAvatarUrl(shop.getShopAvatarUrl());
         return shopDto;
     }
 
@@ -100,6 +101,7 @@ public class ShopServiceImpl implements ShopService {
             return new ShopDto();
         }
         BeanUtils.copyProperties(shop, shopDto);
+        shopDto.setAvatarUrl(shop.getShopAvatarUrl());
         return shopDto;
     }
 
