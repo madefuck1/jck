05e9f6b7195f2cf2953133a4f5711137e22ae1ab
29569 <1778622334@qq.com>
2019-07-02 09:43:03 +0800

    xu

--- app/src/main/java/com/soufang/app/controller/AppPushController.java
+++ app/src/main/java/com/soufang/app/controller/AppPushController.java
@@ -3,6 +3,7 @@ package com.soufang.app.controller;
 import com.soufang.app.feign.AppPushFeign;
 import com.soufang.app.vo.push.PushVo;
 import com.soufang.base.dto.push.PushDto;
+import com.soufang.base.dto.user.UserDto;
 import com.soufang.base.page.PageHelp;
 import com.soufang.base.search.push.PushSo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,17 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("app/push/")
-public class AppPushController {
+public class AppPushController extends AppBaseController{
 
     @Autowired
     AppPushFeign appPushFeign;
 
     //获取推送列表
     @ResponseBody
-    @RequestMapping(value = "getPurchaseList",method = RequestMethod.POST)
+    @RequestMapping(value = "getList",method = RequestMethod.POST)
     public PushVo getPurchaseList(@RequestBody PushSo pushSo, HttpServletRequest request){
+        UserDto userInfo = this.getUserInfo(request);
+        pushSo.setUserId(userInfo.getUserId());
         PageHelp<PushDto> pageHelp = appPushFeign.getList(pushSo);
         PushVo vo = new PushVo();
         vo.setCount(pageHelp.getCount());
