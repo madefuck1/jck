594b23334801341b56ab292f91a60eb3a19b066a
ChenJevon <504987307@qq.com>
2019-07-08 18:03:29 +0800

    bug

--- app/src/main/java/com/soufang/app/feign/AppPurchaseFeign.java
+++ app/src/main/java/com/soufang/app/feign/AppPurchaseFeign.java
@@ -1,16 +1,12 @@
 package com.soufang.app.feign;
 
-import com.soufang.app.vo.purchase.addPurchaseVo;
 import com.soufang.base.dto.enquiry.EnquiryDto;
 import com.soufang.base.dto.purchase.PurchaseDto;
-import com.soufang.base.page.PageHelp;
 import com.soufang.base.search.purchase.PurchaseSo;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @FeignClient("core")
 public interface AppPurchaseFeign {
