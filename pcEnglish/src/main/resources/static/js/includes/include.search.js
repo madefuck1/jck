af3ac5716a4854365e624f01da93f5ffbe24e863
GodSmail <330396052@qq.com>
2019-07-23 11:29:12 +0800

    pcenglish-新闻资讯/搜索框回车查询/热门产品详情查看

--- pcEnglish/src/main/resources/static/js/includes/include.search.js
+++ pcEnglish/src/main/resources/static/js/includes/include.search.js
@@ -28,3 +28,8 @@ $(".tog_in").find('li').hover(function () {
     $(this).css("background-color", "");
 });
 
+document.onkeydown = function(e) {
+    if (e.keyCode == 13) {
+        document.getElementById("btn_search").onclick();
+    }
+}
