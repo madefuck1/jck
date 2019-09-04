5b7fb769a0dce7e6ee69dc4dd3d9dd4221aecee2
1778622334 <1778622334@qq.com>
2019-09-04 11:34:09 +0800

    xu 0904 4

---
+++ app/src/main/java/com/soufang/app/vo/user/OauthVo.java
@@ -0,0 +1,23 @@
+package com.soufang.app.vo.user;
+
+public class OauthVo {
+
+    private String openid ;
+    private String code;
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
